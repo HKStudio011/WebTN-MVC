2d28b4a84cedadcf22da5b94789a9854b0c5b7bf
HKStudio011 <trucngonghinh2002@gmail.com>
2024-03-11 23:33:25 +0700

    add summernote

---
+++ Models/Summernote.cs
@@ -0,0 +1,27 @@
+namespace WebTN_MVC.Models
+{
+    public class Summernote
+    {
+        public Summernote(string iDEditor, bool loadLibrary = true)
+        {
+            IDEditor = iDEditor;
+            LoadLibrary = loadLibrary;
+        }
+
+        public string IDEditor { get; set; }
+        public bool LoadLibrary { get; set; } = true;
+
+        public int Height { get; set; } = 200;
+        public string Toolbar { get; set; } = @"
+            [
+                ['style', ['style']],
+                ['font', ['bold', 'underline', 'clear']],
+                ['color', ['color']],
+                ['para', ['ul', 'ol', 'paragraph']],
+                ['table', ['table']],
+                ['insert', ['link', 'picture', 'video']],
+                ['view', ['fullscreen', 'codeview', 'help']]
+            ]
+        ";
+    }
+}
